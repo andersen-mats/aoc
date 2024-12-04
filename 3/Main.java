0b1bd2d9d29a066f598f416edb74ed42e42d8455
mats <andersenmats@pm.me>
2024-12-04 15:52:11 +0100

    day3 second part

--- 3/Main.java
+++ 3/Main.java
@@ -9,14 +9,35 @@ class Main {
         String linje;
         String[] temp = new String[2];
         int sum = 0;
-
+        int sum2 = 0;
         while (leser.hasNextLine()) {
             linje = leser.nextLine();
             temp = linje.split(",");
             sum += Integer.parseInt(temp[0]) * Integer.parseInt(temp[1]);
         }
 
-        System.out.println(sum);
+        System.out.println("Sum1: " + sum);
+
+        File file2 = new File("matches3.txt");
+        Scanner leser2 = new Scanner(file2);
+        linje = "";
+        boolean mul = true;
+
+        while (leser2.hasNextLine()) {
+            linje = leser2.nextLine();
+            if (linje.equals("don't")) {
+                mul = false;
+            } else if (linje.equals("do")) {
+                mul = true;
+            } else {
+                if (mul) {
+                    temp = linje.split(",");
+                    sum2 += Integer.parseInt(temp[0]) * Integer.parseInt(temp[1]);
+                }
+            }
+        }
+
+        System.out.println("sum2: " + sum2);
     }
 }
             
--- 3/regex.sh
+++ 3/regex.sh
@@ -1,2 +1,4 @@
 grep -oE 'mul\([1-9][0-9]{0,2}\,[1-9][0-9]{0,2}\)' data.txt > matches.txt
 
+grep -oE '[1-9][0-9]{0,2},[1-9][0-9]{0,2}' matches.txt > matches2.txt
+
